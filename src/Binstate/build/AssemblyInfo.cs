3cb7d9edee61cd5ce64ae0daddbaa44debad7599
Ed-ward <ed.pavlov@gmail.com>
2020-05-03 23:55:25 +0200

    Add AssemblyInfo.cs due to it's the easiest way to autogenerate build numbers on TeamCity

---
+++ src/Binstate/build/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle("Binstate")]
+[assembly: AssemblyProduct("Binstate")]
+[assembly: AssemblyCopyright("Copyright © 2020 Ed Pavlov")]
+[assembly: AssemblyDescription("Hierarchical state machine implementation")]
+
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+

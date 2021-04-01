2fe5e4d77a67f28e9597110e8fbc5891e6f8f498
Ed.ward <ed.pavlov@gmail.com>
2021-04-01 21:22:55 +0200

    Implement building and running tests via github action

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
@@ -10,10 +10,6 @@ using System.Runtime.InteropServices;using System.Security.Cryptography.X509Cert
 [assembly: AssemblyProduct("Binstate")]
 [assembly: AssemblyCopyright("Copyright © 2020 Ed Pavlov")]
 [assembly: AssemblyDescription("Hierarchical state machine implementation")]
-
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
 
 [assembly: InternalsVisibleTo("Binstate.Tests, PublicKey=00240000048000009400000006020000002400" +
                               "00525341310004000001000100e1038d45baccd639f23b17bd2105dd590f53ed" +
